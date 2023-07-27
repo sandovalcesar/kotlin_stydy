de4e610cb0729d3d0e12c3adac83e52517e4e852
sandovalcesar <cesar.sandoval.soto@icloud.com>
2023-07-27 17:30:33 -0400

    desde 0

---
+++ src/main/kotlin/variables.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    var titulo = "The Good Code" //TODO variable inferido
+    var subtitulo: String //TODO variable asignada
+
+    subtitulo = "Academia online"
+
+    println("Titulo: " + titulo)
+    println("Subtitulo: " + subtitulo)
+}
---
+++ src/main/kotlin/variables_val.kt
@@ -0,0 +1,9 @@
+fun main (args: Array<String>) {
+
+    val mensaje = "Mundo" //TODO variable read only "val"
+    val mensajeCompleto = "Hola " + mensaje
+    println(mensajeCompleto)
+    /*TODO se usan cuando necesitamos un valor
+     al que necesitamos acceder pero no modificar
+     no es lo mismo que una constante*/
+}
