6631ceb2486de3c809a53aaf7621b7b160a4c1c9
sandovalcesar <cesar.sandoval.soto@icloud.com>
2023-07-27 17:41:50 -0400

    desde 0

---
+++ src/main/kotlin/constantes.kt
@@ -0,0 +1,6 @@
+const val x = 2 //TODO las constantes se definen
+                // fuera del metodo main
+const val titulo = "The Good Code"
+fun main(args: Array<String>) {
+    println(titulo)
+}
