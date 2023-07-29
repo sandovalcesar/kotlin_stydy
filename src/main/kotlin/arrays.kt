fe756311a8a4a931e83f1c3af66b1e8b08210d8d
sandovalcesar <cesar.sandoval.soto@icloud.com>
2023-07-29 13:29:21 -0400

    60%

--- src/main/kotlin/arrays.kt
+++ src/main/kotlin/arrays.kt
@@ -1,8 +1,15 @@
 fun main(args: Array<String>) {
     val arrayPares: IntArray = intArrayOf(0,2,4,6,8)  // TODO array de tipo entero
-    println(arrayPares[1])
-
-    val arrayImpares: IntArray = IntArray(10) {it +2}
+    println(arrayPares[4])
+    println("------------")
+    val arrayImpares: IntArray = IntArray(10) {2} //TODO {constructor}
     println(arrayImpares[0])
     println(arrayImpares[1])
+    println("------------")
+
+    val arrayImparesV2: IntArray = IntArray(15) {it*2} //TODO it = iterador
+    println(arrayImparesV2[0])
+    println(arrayImparesV2[1])
+    println(arrayImparesV2[2])
+    println("------------")
 }
