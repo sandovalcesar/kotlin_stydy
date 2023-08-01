b3b4dc7b2322b847a436d96b90d2530d32eb1c8b
sandovalcesar <cesar.sandoval.soto@icloud.com>
2023-08-01 17:40:15 -0400

    90%

---
+++ src/main/kotlin/whileDowhile.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+
+	var i = 10
+	while (i > 0) {
+		println(i)
+		i--
+	}
+
+	i = 10
+	println("****Do While****")
+
+	do {
+		println(i)
+		i--
+	} while (i > 0)
+
+}
