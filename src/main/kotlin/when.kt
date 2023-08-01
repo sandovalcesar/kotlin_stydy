f9600e4473608ab30b2a38ccb8400fbc4b2768d1
sandovalcesar <cesar.sandoval.soto@icloud.com>
2023-08-01 11:54:47 -0400

    72%

--- src/main/kotlin/when.kt
+++ src/main/kotlin/when.kt
@@ -1,25 +1,21 @@
 fun main(args: Array<String>) {
     val password = "jokspjpcpoa"
 
-    when(password.length){
-        0 -> {
-            println("contraseña vacia")
-        }
-        in 1..4 -> {
-            println("contraseña muy debil")
-        }
-        in 5,6-> {
-            println("contraseña debil")
-        }
-        else -> {
-            println("contraseña segura")
-        }
+    when (password.length) {
+        0 -> { println("contraseña vacia") }
+
+        in 1.rangeTo(4) -> println("contraseña muy debil")
+
+        in 5, 6 -> { println("contraseña debil") }
+
+        else -> { println("contraseña segura") }
     }
 
     esPar(22325)
 
 }
-fun esPar(numero: Int) = when(numero%2){
+
+fun esPar(numero: Int) = when (numero % 2) {
     0 -> println("par")
     else -> println("impar")
 }
